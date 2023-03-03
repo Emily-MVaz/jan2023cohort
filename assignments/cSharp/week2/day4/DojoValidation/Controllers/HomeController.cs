50a59590298b77e43cbc8d7c7938d802f3a2a15f
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-03-03 08:52:15 -0600

    working

--- assignments/cSharp/week2/day4/DojoValidation/Controllers/HomeController.cs
+++ assignments/cSharp/week2/day4/DojoValidation/Controllers/HomeController.cs
@@ -6,26 +6,42 @@ namespace DojoValidation.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public HomeController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public IActionResult Index()
+  {
+    return View();
+  }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  public IActionResult Privacy()
+  {
+    return View();
+  }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
+
+  [HttpGet("/result")]
+  public ViewResult Result()
+  {
+    return View("result");
+  }
+
+    [HttpPost("/result")]
+  public IActionResult CreateUser(string name, string location, string language, string comment)
+  {
+    ViewBag.Name = $"{name}";
+    ViewBag.Location = $"{location}";
+    ViewBag.Language = $"{language}";
+    ViewBag.Comment = $"{comment}";
+    return View("result");
+  }
 }

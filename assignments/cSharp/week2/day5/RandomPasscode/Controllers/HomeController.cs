e943d9d658f0877b12a0f1db52ed490efabb0cf2
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-03-05 16:08:09 -0600

    started random passcode assignment worked on view and controller

--- assignments/cSharp/week2/day5/RandomPasscode/Controllers/HomeController.cs
+++ assignments/cSharp/week2/day5/RandomPasscode/Controllers/HomeController.cs
@@ -6,26 +6,38 @@ namespace RandomPasscode.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public HomeController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public IActionResult Index()
+  {
+    HttpContext.Session.SetInt32("number", 1);
+    int count = Convert.ToInt32(HttpContext.Session.GetInt32("number"));
+    return View(count);
+  }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  public IActionResult Privacy()
+  {
+    return View();
+  }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
+
+  [HttpGet("generate")]
+  public IActionResult Generate()
+  {
+    int count = Convert.ToInt32(HttpContext.Session.GetInt32("number"));
+    count++;
+    HttpContext.Session.SetInt32("number", count);
+    return View("Index", count);  
+  }
+
 }

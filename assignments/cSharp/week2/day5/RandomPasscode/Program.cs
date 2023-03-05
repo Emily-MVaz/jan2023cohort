2b452b9918c9ec60b6ac69635e90f660d8af080f
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-03-05 16:45:09 -0600

    working on passcode generator got it to print but number change needs fixing

--- assignments/cSharp/week2/day5/RandomPasscode/Program.cs
+++ assignments/cSharp/week2/day5/RandomPasscode/Program.cs
@@ -2,19 +2,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+  app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
